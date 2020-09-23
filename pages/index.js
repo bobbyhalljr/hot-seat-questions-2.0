46caf03afccded8e998b308a50767a04a189cc73
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-09-23 02:01:14 -0400

    got the questions on the screen

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,42 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  Icon,
+  List,
+  ListIcon,
+  ListItem,
+  Spinner,
+  Box, 
+  Heading,
+  Flex
+} from '@chakra-ui/core'
 import Layout from '../components/layout'
+import {fakeData} from '../data/fakeData'
+import Question from '../components/question'
 
 export default function Page () {
   return (
     <>
     <Layout>
-      <div>
-        <h1>NextAuth.js Example</h1>
-        <p>
-          This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
-        </p>
-      </div>
+      <Box mx='0 auto'>
+      <Heading mx={4}>Questions</Heading>
+      <Box>
+        <Box>
+          {fakeData.map(data => (
+            <Question 
+            key={data.title}
+            title={data.question}
+            description={data.description}
+            language={data.language}
+            name={data.name}
+            jobTitle={data.jobTitle}
+            src={data.src}
+          />
+          ))}
+        </Box>
+      </Box>
+      </Box>
     </Layout>
     </>
   )
