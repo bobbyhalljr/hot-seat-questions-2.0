01b45f34cf5a51457f4cb4e5a1ba1b4f356675ef
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-09-23 13:51:38 -0400

    added question and index page

--- pages/index.js
+++ pages/index.js
@@ -6,38 +6,60 @@ import {
   List,
   ListIcon,
   ListItem,
-  Spinner,
-  Box, 
+  Button,
+  Box,
   Heading,
-  Flex
+  useColorMode,
 } from '@chakra-ui/core'
-import Layout from '../components/layout'
+import {Main} from '../components/main'
+import { Container } from '../components/Container'
 import {fakeData} from '../data/fakeData'
 import Question from '../components/question'
+import {Hero} from '../components/hero'
+import Link from 'next/link'
 
 export default function Page () {
+  const { colorMode } = useColorMode()
+  const bgColor = {light: 'gray.100', dark: 'gray.700'}
+  const color = {light: 'gray.800', dark: 'white'}
+  // const [ session, { loading: sessionLoading } ] = useSession()
+
   return (
-    <>
-    <Layout>
-      <Box mx='0 auto'>
-      <Heading mx={4}>Questions</Heading>
-      <Box>
-        <Box>
-          {fakeData.map(data => (
-            <Question 
-            key={data.title}
-            title={data.question}
-            description={data.description}
-            language={data.language}
-            name={data.name}
-            jobTitle={data.jobTitle}
-            src={data.src}
-          />
-          ))}
-        </Box>
+    <Container>
+    <Main>
+      <Hero />
+
+      <Heading mt={24} mx={['6', '10']}>
+        How it works 🚀
+      </Heading>
+      <List spacing={['4', '6']} fontSize={['lg', 'xl']} mx={['6', '10']}>
+        <ListItem>
+          <ListIcon icon="check-circle" color="green.500" />
+          Login with your email or github account
+        </ListItem>
+        <ListItem>
+          <ListIcon icon="check-circle" color="green.500" />
+          Post a question
+        </ListItem>
+        <ListItem>
+          <ListIcon icon="check-circle" color="green.500" />
+          View other questions 
+        </ListItem>
+        {/* You can also use custom icons from react-icons */}
+        <ListItem>
+          <ListIcon icon="check-circle" color="green.500" />
+          Be prepared and crush that interview!
+        </ListItem>
+      </List>
+      <Heading mt={24} mx={6} textAlign='center'>So.. what are you waiting for?</Heading>
+      <Box mt={['2', '6']} display='flex' justifyContent='center' alignItems='center'>
+        <Link href="/questions" as="/questions">
+          <a>
+            <Button _hover={{ bg: 'gray.100', color: 'red.500' }}  rounded='lg' shadow='sm' color='white' bg='red.500' fontSize='2xl'>View Questions</Button>
+          </a>
+        </Link>
       </Box>
-      </Box>
-    </Layout>
-    </>
+    </Main>
+    </Container>
   )
 }
--- pages/questions.js
+++ pages/questions.js
@@ -1,30 +1,42 @@
-import Layout from '../components/layout'
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  Icon,
+  List,
+  ListIcon,
+  ListItem,
+  Spinner,
+  Box, 
+  Heading,
+  Flex
+} from '@chakra-ui/core'
+import {Main} from '../components/main'
+import {Container} from '../components/container'
+import {fakeData} from '../data/fakeData'
+import Question from '../components/question'
 
 export default function Page () {
   return (
-    <Layout>
-      <p>
-        This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
-      </p>
-      <h2>Terms of Service</h2>
-      <p>
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
-      </p>
-      <h2>Privacy Policy</h2>
-      <p>
-        This site uses JSON Web Tokens and an in-memory database which resets every ~2 hours.
-      </p>
-      <p>
-        Data provided to this site is exclusively used to support signing in
-        and is not passed to any third party services, other than via SMTP or OAuth for the
-        purposes of authentication.
-      </p>
-    </Layout>
+    <Container>
+      <Main>
+        <Heading mx={4}>Questions</Heading>
+        {/* <Box> */}
+          {/* <Box> */}
+            {fakeData.map(data => (
+              <Question 
+              key={data.title}
+              title={data.question}
+              description={data.description}
+              language={data.language}
+              name={data.name}
+              jobTitle={data.jobTitle}
+              src={data.src}
+              />
+              ))}
+          {/* </Box> */}
+        {/* </Box> */}
+      </Main>
+    </Container>
   )
 }
