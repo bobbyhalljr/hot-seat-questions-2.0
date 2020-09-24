66fc7c6591500aa4b2dd5f2e8afa4e86f3f98e37
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-09-23 23:05:21 -0400

    opt build passing

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,14 @@
-// import Header from '../components/header'
-// import Footer from '../components/footer'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
-// export default function Layout ({children}) {
-//   return (
-//     <>
-//       <Header/>
-//       <main style={{ paddingTop: '6rem', width: '100%' }}>
-//         {children}
-//       </main>
-//       <Footer/>
-//     </>
-//   )
-// }
+export default function Layout ({children}) {
+  return (
+    <>
+      <Header/>
+      <main style={{ paddingTop: '6rem', width: '100%' }}>
+        {children}
+      </main>
+      <Footer/>
+    </>
+  )
+}
