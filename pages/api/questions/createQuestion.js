cbf2c7d4c5939c88119eb8b5b65729a5fe149d71
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-10-04 23:40:24 -0400

    it works, questions refresh when posted

--- pages/api/questions/createQuestion.js
+++ pages/api/questions/createQuestion.js
@@ -1,16 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 
-
-export default async function handler(req, res) {
+export default async (req, res) => {
 const prisma = new PrismaClient();
+try {
     if (req.method === "POST") {
         req.headers = {
             "content-type": "application/json"
         }
         const { body } = req;
         const question = await prisma.post.create({ data: JSON.parse(body) });
-        res.status(200).json(question);
-      }
-    console.log(error.message)
-    // await prisma.$disconnect()
+        res.status(200).json(question)
+        // await prisma.$disconnect()
+    }
+} catch(error){
+    console.log(error)
 }
+}
+
--- pages/api/questions/index.js
+++ pages/api/questions/index.js
@@ -1,16 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 
-
-export default async function handler(req, res){
+export default async (req, res) => {
     const prisma = new PrismaClient()
     req.headers = {
         "content-type": "application/json"
     }
     const questions = await prisma.post.findMany({
+        orderBy: [
+            {
+                createdAt: 'desc'
+            }
+        ],
         include: {
-            user: true
-        }
+            user: true,
+        },
     })
     res.status(200).json(questions)
     await prisma.$disconnect()
 }
+
+export const config = {
+    api: {
+      bodyParser: false,
+    },
+  }
