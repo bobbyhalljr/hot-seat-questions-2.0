d6ea3f7c9b597f46e1a2b0e389ad0f7de15db71f
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2021-01-24 12:09:17 -0500

    blah

--- pages/api/questions/index.js
+++ pages/api/questions/index.js
@@ -1,26 +1,10 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 export default async (req, res) => {
-    const prisma = new PrismaClient()
-    req.headers = {
-        "content-type": "application/json"
-    }
-    const questions = await prisma.post.findMany({
-        orderBy: [
-            {
-                createdAt: 'desc'
-            }
-        ],
-        include: {
-            user: true,
-        },
-    })
-    res.status(200).json(questions)
-    await prisma.$disconnect()
-}
-
-export const config = {
-    api: {
-      bodyParser: false,
-    },
-  }
+  const prisma = new PrismaClient();
+  req.headers = {
+    "content-type": "application/json",
+  };
+  const questions = await prisma.post.findMany();
+  res.status(200).json(questions);
+};
